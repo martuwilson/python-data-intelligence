8e45418b24422da910703994a1ef220c5f45f3ec
Martin Williner <williner@ibm.com>
2025-10-15 11:46:46 -0300

    Add exercises for string manipulation and variable handling in Python

---
+++ Ejercicio1.py
@@ -0,0 +1,29 @@
+""" 
+
+Ejercicio Variables de Texto 1
+Crea dos variables de texto: saludo y nombre.
+
+Asigna el valor "Hola" a saludo y tu nombre a nombre.
+
+Concatena estas dos variables para formar un mensaje de saludo completo y guárdalo en una variable llamada mensajeCompleto. No olvides el espacio entre ambas palabras o el ejercicio te dará error.
+
+Finalmente, imprime mensajeCompleto.
+
+
+
+Nota:
+
+El ejercicio será evaluado por un sistema automático, que considerará la respuesta como incorrecta si hay algun error ortográfico en texto que debes reproducir, o de puntuación, de espacios en blanco adicionales etc. Por favor presta mucha atención a esto.
+
+¿Recibes un error y no sabes por qué? ¿O tal vez no puedes resolver el ejercicio? Al final de cada sección encontrarás las soluciones a los ejercicios de código del día. Te recomiendo revisarlas antes de colocar preguntas en el área de "Preguntas & Respuestas", ya que de esa manera podrás resolver tus dudas mucho más rapidamente que si esperas mi respuesta, que puede demorar hasta 48 horas.
+
+¡Muchos éxitos!
+
+ """
+
+saludo = "Hola"
+nombre = "John Doe"
+
+mensajeCompleto = saludo + " " + nombre
+
+print(mensajeCompleto)
