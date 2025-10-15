390e7ba99e319b3bba3c1491ecbc6b01dfb36c22
Martin Williner <williner@ibm.com>
2025-10-15 12:05:11 -0300

    Numeros en Python + ejercicios

---
+++ Numeros3.py
@@ -0,0 +1,10 @@
+""" 
+
+Declara una variable llamada numero_complejo y almacénale un número complejo de tu elección.
+
+Imprime el tipo de datos de la variable numero_complejo.
+
+ """
+
+numero_complejo = 3 + 4j
+print(type(numero_complejo))
