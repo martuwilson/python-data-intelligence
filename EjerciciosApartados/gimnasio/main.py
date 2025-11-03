e57f7c973c07a161ac35f22459717a28d932f3da
Martin Williner <williner@ibm.com>
2025-11-03 11:18:00 -0300

    Parte 3 - Columnas derivadas

--- EjerciciosApartados/gimnasio/main.py
+++ EjerciciosApartados/gimnasio/main.py
@@ -10,17 +10,17 @@ path= r"C:\Users\004543613\Desktop\python_data_intelligence\EjerciciosApartados\
 df = pd.read_csv(path);
 
 # 3 - Mostrar info general:
-print("\nInformación general del DataFrame:")
+#print("\nInformación general del DataFrame:")
 # Primeras 5 filas
-print('Primeras 5 filas:\n', df.head())
+#print('Primeras 5 filas:\n', df.head())
 #Tipos de datos
-print('\nTipos de datos:\n', df.dtypes)
+#print('\nTipos de datos:\n', df.dtypes)
 # Cantidad de filas y columnas
-print('\nShape (filas, columnas):', df.shape)
+#print('\nShape (filas, columnas):', df.shape)
 # Descripcion estadistica
-print('\nDescripción estadística:\n', df.describe())
+#print('\nDescripción estadística:\n', df.describe())
 # Cantidad de valores nulos
-print('\nValores nulos por columna:\n', df.isnull().sum())
+#print('\nValores nulos por columna:\n', df.isnull().sum())
 
 # PARTE 2: Limpieza de datos
 # 1 - Verificar y rellenar los valores nulos
@@ -39,8 +39,37 @@ df['nombre'] = df['nombre'].astype('string')
 df['plan'] = df['plan'].astype('string')
 
 # re imprimir info para verificar cambios
-print("\nInformación del DataFrame después de la limpieza:")
+#print("\nInformación del DataFrame después de la limpieza:")
 # Ver si hay nulos
-print('\nValores nulos por columna:\n', df.isnull().sum())
+#print('\nValores nulos por columna:\n', df.isnull().sum())
 # Ver tipos de datos
-print('\nTipos de datos:\n', df.dtypes)
+#print('\nTipos de datos:\n', df.dtypes)
+
+# PARTE 3 : Columnas derivadas
+# 1 - calcular imc y agregar como nueva columna
+df['imc'] = df['peso_kg'] / (df['altura_m'] ** 2)
+df['imc'] = df['imc'].round(2)  # Redondear a 2 decimales
+
+# clasificar IMC segun rangos:
+""" 
+<18.5: “Bajo peso”
+18.5–24.9: “Normal”
+25–29.9: “Sobrepeso”
+>=30: “Obesidad”
+"""
+
+condiciones = [
+    df['imc'] < 18.5,
+    (df['imc'] >= 18.5) & (df['imc'] <= 24.9),
+    (df['imc'] >= 25) & (df['imc'] <= 29.9),
+    df['imc'] >= 30
+]
+
+categorias = ['Bajo peso', 'Normal', 'Sobrepeso', 'Obesidad']
+
+# Agregamos default='Desconocido' para que sea del mismo tipo (string) que las categorías
+df['categoria_imc'] = np.select(condiciones, categorias, default='Desconocido')
+
+# 3 - Calculá una columna antiguedad_meses con cuántos meses lleva cada socio activo:
+df['antiguedad_meses'] = ((pd.Timestamp.now() - df['fecha_registro']).dt.days / 30).astype(int)
+
